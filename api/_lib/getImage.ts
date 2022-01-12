94c182df29a9c5a51ef27f313a061680e22b6e89
ivgtr <ivgtr.me@gmail.com>
2022-01-12 14:49:07 +0900

    :ambulance: fix: AmazonのOGPを取得するためUser-Agentを指定

--- api/_lib/getImage.ts
+++ api/_lib/getImage.ts
@@ -6,7 +6,7 @@ type OgpData = {
 };
 
 export const getImage = async (url: string) => {
-  const resp = await axios.get(url);
+  const resp = await axios.get(url, { headers: { "User-Agent": "Twitterbot/1.0" } });
   const body = resp.data;
   const root = parse(body);
   const metaData = root.querySelectorAll("meta");
