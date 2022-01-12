bf199985f1bffff769c9fd4fd1a9e792af930429
ivgtr <ivgtr.me@gmail.com>
2022-01-12 14:15:44 +0900

    :sparkles: add: 機能の追加

--- api/_lib/getImage.ts
+++ api/_lib/getImage.ts
@@ -1,9 +1,35 @@
 import axios from "axios";
+import { parse } from "node-html-parser";
+
+type OgpData = {
+  image?: string;
+};
 
 export const getImage = async (url: string) => {
   const resp = await axios.get(url);
   const body = resp.data;
+  const root = parse(body);
+  const metaData = root.querySelectorAll("meta");
+  const ogpData: OgpData = metaData.reduce((acc: OgpData, meta) => {
+    const prop = meta.getAttribute("property");
+    if (prop === "og:image") {
+      const content = meta.getAttribute("content");
 
+      acc.image = content;
+    }
+    return acc;
+  }, {});
 
-  return { body };
+  if (!ogpData.image) throw new Error("og:imageが見つかりませんでした");
+
+  const { buffer, type } = await axios
+    .get(ogpData.image, { responseType: "arraybuffer" })
+    .then((response) => {
+      return {
+        buffer: response.data,
+        type: response.headers["content-type"].split("/")[1],
+      };
+    });
+
+  return { url: ogpData.image, type, buffer };
 };
